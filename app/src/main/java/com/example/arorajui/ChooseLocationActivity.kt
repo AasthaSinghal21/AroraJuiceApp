bed3f3ffe49c23bd1777d72cf92777fb9d20f74c
simmithapad <simmithapad23@gmail.com>
2024-09-04 12:16:26 +0530

    location activity added

--- app/src/main/java/com/example/arorajui/ChooseLocationActivity.kt
+++ app/src/main/java/com/example/arorajui/ChooseLocationActivity.kt
@@ -1,26 +1,26 @@
 package com.example.arorajui
 
-import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.arorajui.databinding.ActivityChooseLocationBinding
-import com.example.arorajui.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
-    private val binding:ActivityLoginBinding by lazy {
-        ActivityLoginBinding.inflate(layoutInflater)
+class ChooseLocationActivity : AppCompatActivity() {
+    private val binding: ActivityChooseLocationBinding by lazy {
+        ActivityChooseLocationBinding.inflate(layoutInflater)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
         setContentView(binding.root)
-        binding.loginbutton.setOnClickListener{
-            val intent = Intent(this,ChooseLocationActivity::class.java)
-            startActivity(intent)
-        }
+        val locationList = arrayOf("Kaveri Hostel","Krishna Hostel")
+        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1, locationList)
+        val autoCompleteTextView = binding.listOfLocation
+        autoCompleteTextView.setAdapter(adapter)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_choose_location)
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
